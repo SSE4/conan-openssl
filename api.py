7c4af83d8dec60d2275a25655acb3c867e7700cf
SSE4 <tomskside@gmail.com>
2019-06-25 08:27:58 +0200

    - report some python information to compare [skip travis]
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- api.py
+++ api.py
@@ -1,8 +1,37 @@
+from __future__ import print_function
 import requests
 from requests.auth import HTTPBasicAuth
 import os
 import time
 import sys
+import ssl
+import platform
+import ctypes
+
+print("sys version", sys.version_info)
+print("platform version", platform.python_version())
+print("SSL version", ssl.OPENSSL_VERSION)
+
+
+class _OSVERSIONINFOEXW(ctypes.Structure):
+    _fields_ = [('dwOSVersionInfoSize', ctypes.c_ulong),
+                ('dwMajorVersion', ctypes.c_ulong),
+                ('dwMinorVersion', ctypes.c_ulong),
+                ('dwBuildNumber', ctypes.c_ulong),
+                ('dwPlatformId', ctypes.c_ulong),
+                ('szCSDVersion', ctypes.c_wchar * 128),
+                ('wServicePackMajor', ctypes.c_ushort),
+                ('wServicePackMinor', ctypes.c_ushort),
+                ('wSuiteMask', ctypes.c_ushort),
+                ('wProductType', ctypes.c_byte),
+                ('wReserved', ctypes.c_byte)]
+
+os_version = _OSVERSIONINFOEXW()
+os_version.dwOSVersionInfoSize = ctypes.sizeof(os_version)
+if hasattr(ctypes, "windll"):
+    retcode = ctypes.windll.Ntdll.RtlGetVersion(ctypes.byref(os_version))
+
+    print(os_version)
 
 def do_sleep(count):
     for i in range(0, count):
